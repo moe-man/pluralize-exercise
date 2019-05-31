4426c1ccaa02393eedd1b7e3e9e1d07b1e1c11b6
Maurice Nock <mlnock@my.waketech.edu>
2019-05-31 00:16:46 +0000

    pluralize

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var words = ['dog', 'person', 'moose', 'ox', 'wife', 'guest', 'man', 'woman', 'mess', 'photo', 'avocado', 'superhero', 'bar', 'monkey', 'combo', 'toy', 'chair', 'toe'];
 
+var pluralize = require('pluralize')
+
 function getPluralization(word) {
     // This needs to be replaced with the code that returns a pluralized version of the input word
+    word = pluralize(word)
     return word;
 }
 
